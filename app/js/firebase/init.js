edd83b4ce9cdde791b4dc234a10c40c0b800dfd7
Frank Mayer <frank.mayer19072001@outlook.de>
2020-07-18 12:41:55 +0200

    Initial commit

---
+++ app/js/firebase/init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDt49XBPxl_RpcqvV6CXfN6mBFyZG-QlWM",
+    authDomain: "hexkraken.firebaseapp.com",
+    databaseURL: "https://hexkraken.firebaseio.com",
+    projectId: "hexkraken",
+    storageBucket: "hexkraken.appspot.com",
+    messagingSenderId: "1065396952293",
+    appId: "1:1065396952293:web:e5384926ec4a7f486b880c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
